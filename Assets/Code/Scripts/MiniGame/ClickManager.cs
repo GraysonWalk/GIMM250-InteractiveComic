67d9d29131b2977c8d4a0543c1acce6719d534e7
GraysonWalk <graysonwalker172@u.boisestate.edu>
2026-05-03 03:21:41 -0600

    Click Manager, Mouse Attach, and UserGrabber added
    fixed an error where the other three files weren't uploaded

---
+++ Assets/Code/Scripts/MiniGame/ClickManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClickManager : MonoBehaviour
+{
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+
+            Ray ray = cam.ScreenPointToRay(mousePos);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Debug.Log("Hit: " + hit.collider.name);
+
+                hit.collider.GetComponent<Clickable>()?.Click();
+            }
+        }
+    }
+}
